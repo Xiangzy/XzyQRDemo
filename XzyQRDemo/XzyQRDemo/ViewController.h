622b8289d22f679b80c7aeecebb2b9e1f8f32d68
xiangziyan <937864794@qq.com>
2016-08-09 13:39:47 +0800

    新建项目.

---
+++ XzyQRDemo/XzyQRDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XzyQRDemo
+//
+//  Created by  xiangziyan on 16/8/9.
+//  Copyright © 2016年 XiangZiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XzyQRDemo/XzyQRDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XzyQRDemo
+//
+//  Created by  xiangziyan on 16/8/9.
+//  Copyright © 2016年 XiangZiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
